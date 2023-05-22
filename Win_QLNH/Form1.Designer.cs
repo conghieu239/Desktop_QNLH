cc1845b15bba90b94787f4f37b09f4369d12130b
Hieu <conghieu239@gmail.com>
2023-05-22 23:57:09 +0700

    main

---
+++ Win_QLNH/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Win_QLNH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelTrangChu = new System.Windows.Forms.Panel();
+            this.btnKhac = new System.Windows.Forms.Button();
+            this.btnPhieuNhapHang = new System.Windows.Forms.Button();
+            this.btnPhieuDoanhThu = new System.Windows.Forms.Button();
+            this.btnOrder = new System.Windows.Forms.Button();
+            this.btnPXH = new System.Windows.Forms.Button();
+            this.btnQLNV = new System.Windows.Forms.Button();
+            this.btnLTKH = new System.Windows.Forms.Button();
+            this.btnTrangChu = new System.Windows.Forms.Button();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.panelTrangChu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTrangChu
+            // 
+            this.panelTrangChu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.panelTrangChu.Controls.Add(this.btnKhac);
+            this.panelTrangChu.Controls.Add(this.btnPhieuNhapHang);
+            this.panelTrangChu.Controls.Add(this.btnPhieuDoanhThu);
+            this.panelTrangChu.Controls.Add(this.btnOrder);
+            this.panelTrangChu.Controls.Add(this.btnPXH);
+            this.panelTrangChu.Controls.Add(this.btnQLNV);
+            this.panelTrangChu.Controls.Add(this.btnLTKH);
+            this.panelTrangChu.Controls.Add(this.btnTrangChu);
+            this.panelTrangChu.Controls.Add(this.panelLogo);
+            this.panelTrangChu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelTrangChu.Location = new System.Drawing.Point(12, 0);
+            this.panelTrangChu.Name = "panelTrangChu";
+            this.panelTrangChu.Size = new System.Drawing.Size(200, 594);
+            this.panelTrangChu.TabIndex = 0;
+            // 
+            // btnKhac
+            // 
+            this.btnKhac.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.btnKhac.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnKhac.FlatAppearance.BorderSize = 0;
+            this.btnKhac.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnKhac.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnKhac.Image = global::Win_QLNH.Properties.Resources.TrangChu;
+            this.btnKhac.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnKhac.Location = new System.Drawing.Point(0, 448);
+            this.btnKhac.Name = "btnKhac";
+            this.btnKhac.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnKhac.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnKhac.Size = new System.Drawing.Size(200, 50);
+            this.btnKhac.TabIndex = 8;
+            this.btnKhac.Text = "   Khác";
+            this.btnKhac.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnKhac.UseVisualStyleBackColor = false;
+            // 
+            // btnPhieuNhapHang
+            // 
+            this.btnPhieuNhapHang.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.btnPhieuNhapHang.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPhieuNhapHang.FlatAppearance.BorderSize = 0;
+            this.btnPhieuNhapHang.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPhieuNhapHang.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnPhieuNhapHang.Image = global::Win_QLNH.Properties.Resources.Nhap_hang;
+            this.btnPhieuNhapHang.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPhieuNhapHang.Location = new System.Drawing.Point(0, 398);
+            this.btnPhieuNhapHang.Name = "btnPhieuNhapHang";
+            this.btnPhieuNhapHang.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnPhieuNhapHang.Size = new System.Drawing.Size(200, 50);
+            this.btnPhieuNhapHang.TabIndex = 7;
+            this.btnPhieuNhapHang.Text = "   Phiếu nhập hàng";
+            this.btnPhieuNhapHang.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPhieuNhapHang.UseVisualStyleBackColor = false;
+            // 
+            // btnPhieuDoanhThu
+            // 
+            this.btnPhieuDoanhThu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.btnPhieuDoanhThu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPhieuDoanhThu.FlatAppearance.BorderSize = 0;
+            this.btnPhieuDoanhThu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPhieuDoanhThu.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnPhieuDoanhThu.Image = global::Win_QLNH.Properties.Resources.Phieu_doanh_thu;
+            this.btnPhieuDoanhThu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPhieuDoanhThu.Location = new System.Drawing.Point(0, 348);
+            this.btnPhieuDoanhThu.Name = "btnPhieuDoanhThu";
+            this.btnPhieuDoanhThu.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnPhieuDoanhThu.Size = new System.Drawing.Size(200, 50);
+            this.btnPhieuDoanhThu.TabIndex = 6;
+            this.btnPhieuDoanhThu.Text = "   Phiếu doanh thu";
+            this.btnPhieuDoanhThu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPhieuDoanhThu.UseVisualStyleBackColor = false;
+            // 
+            // btnOrder
+            // 
+            this.btnOrder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.btnOrder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnOrder.FlatAppearance.BorderSize = 0;
+            this.btnOrder.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOrder.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnOrder.Image = global::Win_QLNH.Properties.Resources.Order;
+            this.btnOrder.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnOrder.Location = new System.Drawing.Point(0, 298);
+            this.btnOrder.Name = "btnOrder";
+            this.btnOrder.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnOrder.Size = new System.Drawing.Size(200, 50);
+            this.btnOrder.TabIndex = 5;
+            this.btnOrder.Text = "   Order đồ ăn";
+            this.btnOrder.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnOrder.UseVisualStyleBackColor = false;
+            // 
+            // btnPXH
+            // 
+            this.btnPXH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.btnPXH.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnPXH.FlatAppearance.BorderSize = 0;
+            this.btnPXH.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPXH.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnPXH.Image = global::Win_QLNH.Properties.Resources.Xuat_hang;
+            this.btnPXH.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnPXH.Location = new System.Drawing.Point(0, 248);
+            this.btnPXH.Name = "btnPXH";
+            this.btnPXH.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnPXH.Size = new System.Drawing.Size(200, 50);
+            this.btnPXH.TabIndex = 4;
+            this.btnPXH.Text = "   Phiếu xuất hàng";
+            this.btnPXH.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnPXH.UseVisualStyleBackColor = false;
+            // 
+            // btnQLNV
+            // 
+            this.btnQLNV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.btnQLNV.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnQLNV.FlatAppearance.BorderSize = 0;
+            this.btnQLNV.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnQLNV.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnQLNV.Image = global::Win_QLNH.Properties.Resources.QLNV;
+            this.btnQLNV.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnQLNV.Location = new System.Drawing.Point(0, 198);
+            this.btnQLNV.Name = "btnQLNV";
+            this.btnQLNV.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnQLNV.Size = new System.Drawing.Size(200, 50);
+            this.btnQLNV.TabIndex = 3;
+            this.btnQLNV.Text = "   Quản lý nhân viên";
+            this.btnQLNV.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnQLNV.UseVisualStyleBackColor = false;
+            // 
+            // btnLTKH
+            // 
+            this.btnLTKH.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.btnLTKH.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnLTKH.FlatAppearance.BorderSize = 0;
+            this.btnLTKH.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLTKH.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnLTKH.Image = global::Win_QLNH.Properties.Resources.The_KH;
+            this.btnLTKH.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnLTKH.Location = new System.Drawing.Point(0, 148);
+            this.btnLTKH.Name = "btnLTKH";
+            this.btnLTKH.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnLTKH.Size = new System.Drawing.Size(200, 50);
+            this.btnLTKH.TabIndex = 2;
+            this.btnLTKH.Text = "   Lập thẻ khách hàng\r\n";
+            this.btnLTKH.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLTKH.UseVisualStyleBackColor = false;
+            // 
+            // btnTrangChu
+            // 
+            this.btnTrangChu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(76)))));
+            this.btnTrangChu.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnTrangChu.FlatAppearance.BorderSize = 0;
+            this.btnTrangChu.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrangChu.ForeColor = System.Drawing.Color.Gainsboro;
+            this.btnTrangChu.Image = global::Win_QLNH.Properties.Resources.TrangChu;
+            this.btnTrangChu.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnTrangChu.Location = new System.Drawing.Point(0, 98);
+            this.btnTrangChu.Name = "btnTrangChu";
+            this.btnTrangChu.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnTrangChu.Size = new System.Drawing.Size(200, 50);
+            this.btnTrangChu.TabIndex = 1;
+            this.btnTrangChu.Text = "   Trang chủ";
+            this.btnTrangChu.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnTrangChu.UseVisualStyleBackColor = false;
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackgroundImage = global::Win_QLNH.Properties.Resources.LOGO1;
+            this.panelLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelLogo.Location = new System.Drawing.Point(0, 0);
+            this.panelLogo.Name = "panelLogo";
+            this.panelLogo.Size = new System.Drawing.Size(200, 98);
+            this.panelLogo.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 594);
+            this.Controls.Add(this.panelTrangChu);
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.Text = "Form1";
+            this.panelTrangChu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelTrangChu;
+        private System.Windows.Forms.Button btnTrangChu;
+        private System.Windows.Forms.Panel panelLogo;
+        private System.Windows.Forms.Button btnKhac;
+        private System.Windows.Forms.Button btnPhieuNhapHang;
+        private System.Windows.Forms.Button btnPhieuDoanhThu;
+        private System.Windows.Forms.Button btnOrder;
+        private System.Windows.Forms.Button btnPXH;
+        private System.Windows.Forms.Button btnQLNV;
+        private System.Windows.Forms.Button btnLTKH;
+    }
+}
+
